ea3e7d445d7c6c9ee4e1e724715a1e0ef2eb8c84
Ras-AlGhoul <yazann.abbas@gmail.com>
2021-08-31 19:42:40 +0200

    getch and app design

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,30 @@
-// alert('whats up G!')
+console.log('client side is up and running');
+const searchForm = document.querySelector('form');
+const search = document.querySelector('input');
+const button = document.querySelector('button');
+const results = document.getElementById('results');
+
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(search.value);
+    fetch(`http://localhost:3000/weather?address=${search.value}`)
+        .then(res => {
+            res.json().then(data => {
+                if (data.error) {
+                    results.innerHTML = data.error;
+                } else {
+
+                    results.innerHTML = data.forecastData;
+                }
+            })
+        })
+        .catch(error => console.log(error));
+
+})
+
+// searchForm.addEventListener('submit', (e)=>{
+//     e.preventDefault();
+//     console.log('Ive been clicked',searchForm);
+// })
+
